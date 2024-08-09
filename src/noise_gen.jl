39dc16f7d1ee48001f993d5829b7735327ae8dc7
Enki A. Barra M <enki.barmel97@ciencias.unam.mx>
2024-08-08 20:53:45 -0600

    feat: added .phony targets to plot trazes and psd of corr noises & to clean workspace.

--- src/noise_gen.jl
+++ src/noise_gen.jl
@@ -55,8 +55,3 @@ function intercept_and_exponent(x::Vector{Float64},y::Vector{Float64})::Vector{F
    data = DataFrame(X=x,Y=y)
    return GLM.coef(GLM.lm(@formula(Y ~ X), data))
 end
-
-
-function movingaverage(ts::Vector{Float64})::Vector{Float64}
-   return 
-end
--- src/plots/plot_psd.jl
+++ src/plots/plot_psd.jl
@@ -13,15 +13,19 @@ function plot_psd(x::Vector{Float64}, destination_dir::String, A::Float64, beta:
     psd = compute_psd(rfft_arr)
     deleteat!(psd,1)
 
-    params = intercept_and_exponent(f,psd)
+    params = intercept_and_exponent(log10.(f),log10.(psd))
     
     full_file_path = joinpath(destination_dir,"psd_log_A_$(replace(string(round(A,digits=2)),"." => "_"))_beta_$(replace(string(round(beta,digits=2)),"." => "_")).pdf")
     if !isfile(full_file_path)
         #plot styling
-        plt = plot(f,psd, label=L"PSD \ \left( f \right)", legend=false, xscale=:log10, yscale=:log10,alpha=0.2) #plot reference 
+        plt = plot(f,psd, label=L"PSD \ \left( f \right)", legend=false, xscale=:log10, yscale=:log10,alpha=0.2) #plot reference
+
         #expected linear fit
-        plot!((u) -> exp10(log10(A)-beta*log10(u)), minimum(f), maximum(f), xscale=:log10,yscale=:log10,lc=:black)
+        plot!((u) -> A*u^-beta, minimum(f), maximum(f), xscale=:log10,yscale=:log10,lc=:black)
         
+        #linear fit
+        plot!((u) -> exp10(params[1] + params[2]*log10(u)),minimum(f),maximum(f), xscale=:log10,yscale=:log10,lc=:red)
+
         title!("PSD for ts with A = $A and beta = $beta")
         xlabel!(L"f")
         ylabel!("power density spectra")
